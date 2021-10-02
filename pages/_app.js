4222e2d97d6766f6c157c6a182262ec7670c48ab
manuel9a40 <mcai@ic-itcr.ac.cr>
2021-10-02 04:14:41 -0600

    Correción errores y warnings

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../styles/leaflet.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import '../public/leaflet.css'
 import Script from 'next/script'
 
 export default function Home() {
