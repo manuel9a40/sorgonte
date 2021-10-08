88d59f8ecf81680d5da4915cf6b28069b34de619
manuel9a40 <mcai@ic-itcr.ac.cr>
2021-10-08 16:55:19 -0600

    Cambiar ruta por defecto de los íconos

--- public/index1.js
+++ public/index1.js
@@ -1,4 +1,7 @@
 window.onload = () => {
+  L.Icon.Default.prototype.options.iconUrl = "images/marker-icon.png";
+  L.Icon.Default.prototype.options.iconRetinaUrl = "images/marker-icon-2x.png";
+  L.Icon.Default.prototype.options.shadowUrl = "images/marker-shadow.png";
   var map = L.map('map').setView([9.911657797094934, -84.02942419052123], 18);
   L.tileLayer('https://{s}.tile.osm.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'}).addTo(map);
   fetch('/output.json').then(response => {
